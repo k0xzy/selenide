6e05a67cefa182699bdf4a48a5b57889c9245943
k0xzy <koxzy22@gmail.com>
2022-10-06 18:54:28 +0300

    updtDzSelenide

--- src/test/java/ru/netology/RegistrationTest.java
+++ src/test/java/ru/netology/RegistrationTest.java
@@ -1,23 +1,38 @@
 package ru.netology;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Condition;
+import org.openqa.selenium.Keys;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
-import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 class RegistrationTest {
 
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+
     @Test
     void test() {
+        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         $x("//input[@placeholder=\"Город\"]").setValue("Москва");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        String planningDate = generateDate(4);
+        $("[placeholder=\"Дата встречи\"]").setValue(planningDate);
         $("[name=\"name\"]").setValue("Иванов Иван");
         $("[name=\"phone\"]").setValue("+79999991111");
         $x("//*[@class=\"checkbox__box\"]").click();
         $x("//*[@class=\"button__text\"]").click();
-        $x("//*[contains(text(),\"Встреча успешно забронирована на\")]").should(visible, Duration.ofSeconds(15));
+        $x("//div[text()= 'Успешно!']").should(Condition.visible, Duration.ofSeconds(15));
+        $x("//div[contains(text(), 'Встреча успешно забронирована')]").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
     }
 }
